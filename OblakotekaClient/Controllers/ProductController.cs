1a2bf7ce6d1f089df397ca0031339a8bf46007ef
gfg7 <unakonechnaya@yandex.com>
2024-04-02 00:40:43 +0500

    init client

---
+++ OblakotekaClient/Controllers/ProductController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OblakotekaClient.Controllers;
+
+public class ProductController : Controller
+{
+
+    public ProductController()
+    {
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
