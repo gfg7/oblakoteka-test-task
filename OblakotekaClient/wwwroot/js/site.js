433f46cddd65423a4ab08cdb7c95aa3050369c6c
gfg7 <unakonechnaya@yandex.com>
2024-04-03 03:26:06 +0500

    add product llst & search

--- OblakotekaClient/wwwroot/js/site.js
+++ OblakotekaClient/wwwroot/js/site.js
@@ -1,4 +1,30 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿$(document).ready(function() {
+    GetProducts();
+});
 
-// Write your JavaScript code.
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+  });
+
+$("#search-bar").on('keyup', function() {
+        GetProducts($('#search-bar').val());
+});
+
+function GetProducts(search) {
+    $.ajax({
+        url: `/product/find?search=${search ?? ''}`,
+        type: 'GET', 
+        beforeSend: function() {
+            $('.spinner-border').show();
+        },
+        complete: function() {
+            $('.spinner-border').hide();
+        },
+        success: function(result) {
+            $('.product-table').html(result);
+        },
+        error: function(xhr, status, error) {
+            console.log(error); 
+        }
+    });
+}
