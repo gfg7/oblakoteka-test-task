fb9bf2476a9f60643cac7587c453b37146671c9d
gfg7 <unakonechnaya@yandex.com>
2024-04-03 07:05:24 +0500

    add modal

--- OblakotekaClient/wwwroot/js/site.js
+++ OblakotekaClient/wwwroot/js/site.js
@@ -28,3 +28,67 @@ function GetProducts(search) {
         }
     });
 }
+
+function DeleteProduct(id) {
+    $.ajax({
+        url: `/product/${id}`,
+        type: 'DELETE', 
+        success: function(result) {
+            GetProducts($('#search-bar').val())
+        },
+        error: function(xhr, status, error) {
+            console.log(error); 
+        }
+    });
+}
+
+function GetProduct(id) {
+    var data = $(`tr[data-id=${id}] td .data`).map(function(){ return this.innerText })
+    $('#editModal input[name=id]').val(id)
+    $('#editModal input[name=Name]').val(data[0])
+    $('#editModal input[name=Description]').val(data[1])
+}
+
+function EditProduct() {
+    $.ajax({
+        url: `/product/${$('#editModal input[name=id]').val()}`,
+        type: 'PUT', 
+        data : $('#editModal form').serialize(),
+        beforeSend: function() {
+            $('#editModal').modal('hide')
+            $('.spinner-border').show();
+        },
+        complete: function() {
+            $('.spinner-border').hide();
+        },
+        success: function(result) {
+            GetProducts($('#search-bar').val());
+        },
+        error: function(xhr, status, error) {
+            console.log(error); 
+        }
+    });
+}
+
+function CreateProduct() {
+    $.ajax({
+        url: `/product/`,
+        type: 'POST', 
+        data : $('#createModal form').serialize(),
+        beforeSend: function() {
+            $('#createModal').modal('hide')
+            $('.spinner-border').show();
+        },
+        complete: function() {
+            $('.spinner-border').hide();
+        },
+        success: function(result) {
+            GetProducts($('#search-bar').val());
+        },
+        error: function(xhr, status, error) {
+            console.log(error); 
+        }
+    });
+}
+
+  
